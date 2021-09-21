94f260227030b72f445f1d7d0b2dccce41bfbb15
Coienrr <972054003@qq.com>
2021-09-21 11:55:36 +0800

    2.1

---
+++ day2/koa-setup/index.js
@@ -0,0 +1,30 @@
+//esm
+import fs from "fs";
+import { createIndexTemplate } from "./indexTemplate.js";
+import { createPackageTemplate } from "./packageTemplate.js";
+import question from "./question/index.js";
+import { createConfig } from "./config.js";
+import execa from 'execa';
+
+const ans = await question();
+const inputConfig = createConfig(ans);
+
+// happy path
+//1.创建文件夹
+fs.mkdirSync(getRootPath());
+//2.创建index.js
+fs.writeFileSync(getRootPath() + "/index.js", createIndexTemplate(inputConfig));
+//3.创建package.json
+//练习点:基于数据生成 package.json
+fs.writeFileSync(getRootPath() + "/package.json", createPackageTemplate(inputConfig));
+//4.安装依赖
+//TODO package 
+execa("npm i", {
+    cwd: getRootPath(),
+    stdio: [2,2,2],
+})
+
+function getRootPath(){
+    console.log(process.cwd());
+    return "./haha";
+}
