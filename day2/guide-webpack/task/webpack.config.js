ee55cec6d464fd9d1cb5bd86f106f696f88d3307
Coienrr <972054003@qq.com>
2021-09-21 14:10:08 +0800

    2.2

---
+++ day2/guide-webpack/task/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const json5 = require("json5");
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.json5$/i,
+                type: "json",
+                parser: {
+                    parse: json5.parse,
+                },
+            }
+        ]
+    }
+}
